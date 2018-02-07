d0821785efa584982f3efde9769a94b6cf9d901e
RedSPINE <rc.redspine@gmail.com>
2018-02-07 18:29:27 +0100

    First commit, 3 files : fitness, generation, ui

---
+++ fitness.py
@@ -0,0 +1,371 @@
+# !/usr/bin/env python
+# -----------------------------------------------
+__author__ = "Duc Minh TRAN, Tao Duy Chuan LE"
+__version__ = "0.0.1"
+# -----------------------------------------------
+
+import math
+import generation as gen
+import random
+
+# HAND VITESSE (TOUR/Second)
+VITESSE_SECONDHAND = 1. / 60
+VITESSE_MINUTEHAND = 1. / 3600
+VITESSE_HOURHAND = 1. / 43200
+
+# POINTS FOR CONNECTIONS
+GEAR_Z_GEAR = 5
+GEAR_XY_GEAR = 5
+GEAR_Z_HAND = 15
+SPRING_Z_BARREL = 50
+FORK_XY_ESCAPE_WHEEL = 50
+BALANCEWHEEL_XY_FORK = 50
+BARREL_XY_GEAR = 50
+SPRING_Z_BALANCEWHEEL = 50
+GEAR_XY_ESCAPE_WHEEL = 20
+# INITIAL VITESSE
+VITESSE_INITIAL = 1/60
+
+wanted_pieces = {
+	"Barrel" : 1, 
+	"Fork" : 1, 
+	"EscapeWheel" : 1, 
+	"BalanceWheel" : 1,
+	"Spring" : 2,
+	"Hand" : 3
+}
+
+COMPONENT_COST = 3
+UNIQUE = 50
+
+#NUMBER OF GENERATIONS:
+NUMBER_GENERATIONS = 1000
+NUMBER_CROSSING = 100
+
+class Connection():
+    """
+    __init__() functions as the class constructor
+    """
+    def __init__(self, name=None, point=0):
+        self.name = name
+        self.point = point
+
+
+valid_connections = []
+
+
+def input_database():
+	"""
+	Function allows to create a list (more like database) of possible connections in a clock, and give a value to all of them
+	"""
+
+	valid_connections.append(Connection(["Gear", "Gear", gen.Z_CONNECTION], GEAR_Z_GEAR))
+
+	valid_connections.append(Connection(["Gear", "Gear", gen.XY_CONNECTION], GEAR_XY_GEAR))
+
+	valid_connections.append(Connection(["Gear", "Hand", gen.Z_CONNECTION], GEAR_Z_HAND))
+	valid_connections.append(Connection(["Hand", "Gear", gen.Z_CONNECTION], GEAR_Z_HAND))
+
+	valid_connections.append(Connection(["Fork", "EscapeWheel", gen.XY_CONNECTION], FORK_XY_ESCAPE_WHEEL))
+	valid_connections.append(Connection(["EscapeWheel", "Fork", gen.XY_CONNECTION], FORK_XY_ESCAPE_WHEEL))
+
+	valid_connections.append(Connection(["BalanceWheel","Fork", gen.XY_CONNECTION], BALANCEWHEEL_XY_FORK))
+	valid_connections.append(Connection(["Fork", "BalanceWheel", gen.XY_CONNECTION], BALANCEWHEEL_XY_FORK))
+
+	valid_connections.append(Connection(["EscapeWheel", "Gear", gen.XY_CONNECTION],GEAR_XY_ESCAPE_WHEEL))
+	valid_connections.append(Connection(["Gear", "EscapeWheel", gen.XY_CONNECTION], GEAR_XY_ESCAPE_WHEEL))
+
+	valid_connections.append(Connection(["Spring", "Barrel", gen.Z_CONNECTION], SPRING_Z_BARREL))
+	valid_connections.append(Connection(["Barrel", "Spring", gen.Z_CONNECTION], SPRING_Z_BARREL))
+
+	valid_connections.append(Connection(["Spring", "BalanceWheel", gen.Z_CONNECTION], SPRING_Z_BALANCEWHEEL))
+	valid_connections.append(Connection(["BalanceWheel", "Spring", gen.Z_CONNECTION], SPRING_Z_BALANCEWHEEL))
+
+	valid_connections.append(Connection(["Barrel", "Gear", gen.XY_CONNECTION], BARREL_XY_GEAR))
+	valid_connections.append(Connection(["Gear", "Barrel", gen.XY_CONNECTION], BARREL_XY_GEAR))
+
+
+def find_connections(piece, connection_list):
+	# Used by group connections, returns every connection for a given piece, always put the chosen piece before in the connection trinome
+	connections = []
+	for i in range(len(connection_list)):
+		if piece in connection_list[i]:
+			if piece == connection_list[i][1]:
+				connection_list[i][0], connection_list[i][1] = connection_list[i][1], connection_list[i][0]
+			connections += [connection_list[i]]
+	return connections
+
+
+def group_connections(clock):
+	# Returns a big list of connections, redundant, ordered by piece
+	connection_list = gen.list_connections(clock)
+	cn_group = []
+	for piece in clock[1:]:
+		cn_group += [find_connections(piece, connection_list)]
+	return cn_group
+
+
+def find_chains(connection_list):
+	chains = []
+	for cn in connection_list:
+		chain = [cn]
+		piece0 = cn[0]
+		piece1 = cn[1]
+		end_flag = False
+		while not end_flag:
+			end_flag = True
+			for cn1 in connection_list:
+				used_pieces = [x for c in chain for x in c]
+				if (piece0 in cn1 or piece1 in cn1) and (not (piece0 in cn1 and piece1 in cn1)) and (not cn1 in chain): # <=> piece0 in cn1 XOR piece1 in cn1
+					chain += [cn1]
+					piece0 = cn1[0]	
+					piece1 = cn1[1]
+					end_flag = False
+		chains += [chain]
+	return chains
+
+
+def gears_XY_connection(obj1,obj2):
+	""" 
+	2 gears connected by teeths,if gear1 spins will train gear 2 to spin
+	"""
+	if (obj1.y_position == obj2.y_position) and (obj2.x_position == obj1.x_position) and distance_between_objs(obj1, obj2) < PIN_DISTANCE and (obj1.is_rotate() or obj2.is_rotate()):
+		obj2.rotates = True
+		obj2.speed = gear1.speed * gear1.nb_teeth / gear2.nb_teeth
+
+
+def objects_Z_connection(obj1,obj2):
+	"""
+	If 2 objects in the same axe, obj1 will train vitesse to obj2
+	"""
+	if obj1.z_position == obj2.z_position :
+		if obj1.is_rotate():
+			obj2.rotates = True
+			obj2.speed = obj1.speed
+
+
+def is_rotate(obj):
+	if obj.speed != 0:
+		return True
+	return False
+
+
+def initial_clock_run(clock):
+	# INPUT : LIST OF OBJECTS OF A CLOCK
+	# OUTPUT : IF THE CLOCK RUNS< INITIALIZE THE MOVEMENT OF ESCAPE WHEEL.
+	connection_list = gen.list_connections(clock)
+	if does_clock_run(connection_list):
+		for i in range(1, len(clock)):
+			if clock[i].__class__.__name__ == "EscapeWheel":
+				clock[i].speed = VITESSE_INITIAL
+
+
+def is_in_connectdata(connection):
+	# INPUT : A list type [object, object, type of connection]
+	# OUTPUT : A Point correspond with the connection in valid_connections, 0 if the input connection doesn't appear in valid_connections
+	x = [connection[0].__class__.__name__, connection[1].__class__.__name__, connection[2]]
+	for i in range(len(valid_connections)):
+		# THIS STEP COMPARES NAME OF OBJECT1, NAME OF OBJECT2, TYPE OF CONNECTION
+		if (x[0]==(valid_connections[i].name)[0]) and (x[1]==(valid_connections[i].name)[1]) and (x[2]==(valid_connections[i].name)[2]):
+			return valid_connections[i].point
+
+	return (-15)
+
+
+def fitness(clock):
+	"""
+	INPUT : A LIST OF CONNECTION OF A CLOCK
+	OUTPUT: FITNESS SCORE OF THAT CLOCK
+	This part gives score based on connections in a clock that appears in valid_connections
+	"""
+	connection_list = gen.list_connections(clock)
+	groups = group_connections(clock)
+	score = 0
+
+	# Each component reduces the fitness score
+	score = score - (COMPONENT_COST * (len(clock) - 1)) 
+
+	# For each unique piece, give point only if it has one occurence in the watch
+	for name in wanted_pieces:
+		nb_of_this_piece = 0
+		for piece in clock[1:]:
+			if piece.__class__.__name__ == name :
+				nb_of_this_piece += 1
+		if nb_of_this_piece == wanted_pieces[name]:
+			score += UNIQUE
+
+
+	# For each valid connection, give points based on the connection value
+	for cn in connection_list:
+		score += is_in_connectdata(cn)
+	
+	# If a component owns three or more horizontal connexion, it blocks the whole system
+	for piece_cn in groups:
+		nb_xy_cn = 0
+		nb_z_cn = 0
+		for bloc in piece_cn:
+			if bloc[2] == gen.XY_CONNECTION:
+				nb_xy_cn += 1
+			elif bloc[2] == gen.Z_CONNECTION:
+				nb_z_cn += 1
+		if nb_xy_cn <= 2:
+			score += 1
+		else: 
+			score -= 100
+
+	''' # EVALUATE THE CLOCK WHEN IT RUNS
+	if (does_clock_run(connection_list)):
+		initial_clock_runs(clock)
+		# AWARD FOR RUNNING CLOCK
+		score += 100.0
+		# CHECK FOR EACH CONNECTION OF RUNNING CLOCK
+		for cn in connection_list:
+			print("Tick Tock Tick Tock Tick Tock")
+			x = [cn[0].__class__.__name__,cn[1].__class__.__name__,cn[2]]
+			if ((x[0]=="Hand") and (x[1]=="Gear") and (x[2]==XY_CONNECTION)):
+				# IF 2 GEARS CONNECT HORIZONTALLY THEN THE OTHER GEAR WILL SPIN
+				gears_XY_connection(cn[0], cn[1])
+			if ((x[0]=="Hand") and (x[1]=="Gear") and (x[2]==Z_CONNECTION)) or ((x[1]=="Hand") and (x[0]=="Gear") and (x[2]==Z_CONNECTION)) or ((x[0]=="Gear") and (x[1]=="Gear") and (x[2]==Z_CONNECTION)):
+				# IF A GEAR AND A HAND CONNECT VERTICALLY THAN THE HAND WILL SPIN
+				objects_Z_connection(cn[0], cn[1])
+
+			if (cn[1] == gen.Hand):
+				hand = cn[1]
+			if (cn[0] == gen.Hand):
+				hand = cn[0]
+			"""
+			EVALUATE CLOCK
+			"""
+			if (hand.speed < 1./30) and (hand.speed > 1./90): 
+				# IN THIS CASE THE HAND MAYBE THE SECOND HAND, A ROUND WILL TAKE BETWEEN 30s AND 90S
+				SEC_SCORE = 1 - abs((abs(hand.speed) - VITESSE_SECONDHAND) / VITESSE_SECONDHAND)
+				SCORE_HAND = SEC_SCORE
+			elif (hand.speed < 1./1800) and (hand.speed > 1./5400):
+				# IN THIS CASE THE HAND MAYBE THE MINUTE HAND, A ROUND WILL TAKE BETWEEN 1800s( 30 minutes) to 5400s (90 minutes).
+				MIN_SCORE = 1 - abs((abs(hand.speed) - VITESSE_MINUTEHAND) / VITESSE_MINUTEHAND)
+				SCORE_HAND = 60 * MIN_SCORE
+			elif (hand.speed < 1./21600) and (hand.speed > 1./64800):
+				# IN THIS CASE THE HAND MAYBE THE HOUR HAND, A ROUND WILL TAKE BETWEEN 21600S (12H) TO 64800(36H).
+				HOU_SCORE = 1 - abs((abs(hand.speed) - VITESSE_HOURHAND) / VITESSE_HOURHAND)
+				SCORE_HAND = 3600 * HOU_SCORE
+			else : 
+				SCORE_HAND = 0
+			score += SCORE_HAND '''
+
+	return score
+
+
+"""
+The hands are important, the more accurately a hand hand tells time, the better fitness score it gets
+Hourhand > Minute Hand > Second Hand by priority ( we must know what hours, then what minute, then what second)
+"""
+
+
+def fitness_population(population):
+	list_fitness = []
+	for clock in population:
+		list_fitness += [fitness(clock)]
+	return list_fitness
+
+
+def average_fitness(population):
+	sum_fitness = 0
+	for clock in population:
+		sum_fitness += fitness(clock)
+	return sum_fitness/len(population)
+
+
+def evolve(population):
+
+	for i in range(NUMBER_CROSSING):
+		"""
+		Randomly choose 3 clocks from the population
+		Remove the one with the lowest fitness score
+		Mate (evolve) the other two to create a new baby clock
+		Add the new baby clock to the population
+		"""
+
+		# Chose 3 monsters
+		obj1 = population.pop(random.randrange(len(population)))
+		obj2 = population.pop(random.randrange(len(population)))
+		obj3 = population.pop(random.randrange(len(population)))
+
+		# Find the weakest, and remove it
+		a = fitness(obj1)
+		b = fitness(obj2)
+		c = fitness(obj3)
+		if a == min(a,b,c):
+			destroyed_clock = obj1
+			dad_clock = obj2
+			mom_clock = obj3
+		elif b == min(a,b,c):
+			destroyed_clock = obj2
+			dad_clock = obj1
+			mom_clock = obj3
+		else:
+			destroyed_clock = obj3
+			dad_clock = obj2
+			mom_clock = obj1
+
+		# Mate the 2 strongest and give the baby to the population
+		baby_clock = gen.mate(dad_clock,mom_clock, random.randint(0, 100000))
+
+		population.append(dad_clock)
+		population.append(mom_clock)
+		population.append(baby_clock)
+		
+def average_number_of_components(population):
+	sum_components = 0
+	for monster in population:
+		sum_components += len(monster)-1
+	sum_components /= len(population)
+	return sum_components
+
+def natural_selection(population):
+	i=0
+	while(i<NUMBER_GENERATIONS-1):
+		i+=1
+		print("Generation n°", i+1, ":", average_fitness(population), "Nbr pieces : ", average_number_of_components(population), " Best Fitness : ", fitness(best_clock(population)))
+		evolve(population)
+
+
+def best_clock(population):
+	#RETURN THE BEST CLOCK
+	best = population[0]
+	for clock in population:
+		if fitness(clock) > fitness(best) :
+			best = clock
+	return best
+
+def display_clock(clock):
+	connection_list = gen.list_connections(clock)
+	for i in range(0,len(connection_list)):
+		print (connection_list[i][0].__class__.__name__,connection_list[i][1].__class__.__name__,connection_list[i][2])
+
+"""
+	Test
+"""
+
+print(" ------------------------------------------------------- ")
+input_database()
+population = gen.generate(100,3,100)
+natural_selection(population)
+
+best = best_clock(population)
+print("Best fitness : ", fitness(best))
+for piece in gen.classes :
+	counter = 0
+	for part in best[1:]:
+		if isinstance(part, piece):
+			counter+=1
+	print("Number of ", piece, " : ", counter)
+print("------ connections ------")
+print(gen.list_connections(best))
+
+
+''' for i in range(len(valid_connections)) :
+	print(i, " : ", valid_connections[i].name, " -> ", valid_connections[i].point)
+print( "   ",1 - abs((abs(1./60) - VITESSE_SECONDHAND) / VITESSE_SECONDHAND))
+print( "   ",1 - abs((abs(1/.60) - VITESSE_MINUTEHAND) / VITESSE_MINUTEHAND))
+print( "   ",1 - abs((abs(1./60) - VITESSE_HOURHAND) / VITESSE_HOURHAND))
+ '''
---
+++ generation.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+# -----------------------------------------------
+__author__ = "Robin Cochennec"
+__version__ = "0.0.1"
+# Release Notes ---------------------------------
+'''
+09/24/2017 : file creation, add constants
+09/27/2017 : add inheritances, plus some more classes, deep thoughts around binary convertion
+11/23/2017 : patched the axis/hand problem - added axis and functions to convert objects to binary sequence and vice versa
+12/07/2017 : finished conversion functions
+01/11/2018 : created conversion functions for watches
+'''
+# -----------------------------------------------
+
+import random as rnd
+import math as m
+
+
+CHANCE_TO_MUTATE = 1/50
+# Gears module
+GEARS_MODULE = 0.1
+# Possible positions /!\ We're working with natural integers only bits : 11 - 11 - 4
+X_RANGE = 2048 
+Y_RANGE = 2048
+Z_RANGE = 4
+# Number of teeth - 10 bits
+MIN_NB_TEETH = 3
+MAX_NB_TEETH = 1000
+# Objects code - 4 bits
+GEAR_CODE = "0000"
+ESCAPE_WHEEL_CODE = "0001"
+BARREL_CODE = "0010"
+BALANCE_WHEEL_CODE = "0011"
+BALANCE_WHEEL_RAY = 40
+SPRING_CODE = "0100"
+SPRING_RAY = 0
+HAND_CODE = "0101"
+HAND_RAY = 0
+FORK_CODE = "0110"
+FORK_RAY = 30
+BUG_CODE = "1111"
+
+NO_CONNECTION = 0
+XY_CONNECTION = 1
+Z_CONNECTION = 2
+
+PIN_DISTANCE = 20
+
+
+class Object:
+    """
+    An Object is a Father-Class used to give any piece of the watch a position in its relative space
+    code = the binary version of the Object, containing all the needed informations for crossovers and mutations
+    """
+    def __init__(self):
+        self.x_position = rnd.randint(0, X_RANGE)
+        self.y_position = rnd.randint(0, Y_RANGE)
+        self.z_position = rnd.randint(0, Z_RANGE)
+
+        self.ray = 0
+    
+
+
+class Gear(Object):
+    """
+    Definition of a Gear
+    """
+    # Creation
+    def __init__(self):
+        Object.__init__(self)
+        self.nb_teeth = rnd.randint(MIN_NB_TEETH, MAX_NB_TEETH)
+        self.rotates = False
+        self.speed = 0  # In spin by minutes
+
+        self.ray = int(self.nb_teeth * GEARS_MODULE / 2 )
+
+
+class EscapeWheel(Gear):
+    """
+    The escape wheel basically has the same parameters than a gear
+    """
+    # Creation
+    def __init__(self):
+        Gear.__init__(self)
+
+
+class Barrel(Gear):
+    """
+    Definition of a Barrel
+    """
+    # Creation
+    def __init__(self):
+        Gear.__init__(self)
+
+
+class BalanceWheel(Object):
+    """
+    Definition of a Balance Wheel
+    """
+    # Creation
+    def __init__(self):
+        Object.__init__(self)
+        self.ray = BALANCE_WHEEL_RAY
+
+
+class Spring(Object):
+    """
+    Definition of a Mainspring
+    """
+    # Creation
+    def __init__(self):
+        Object.__init__(self)
+        self.ray = SPRING_RAY
+
+class Hand(Object):
+    """
+    Definition of a Hand
+    """
+    # Creation
+    def __init__(self):
+        Object.__init__(self)
+        self.speed = 0  # In spin by minutes
+        self.ray = HAND_RAY
+
+
+class Fork(Object):
+    """
+    Definition of an Fork
+    """
+    # Creation
+    def __init__(self):
+        Object.__init__(self)
+        self.ray = FORK_RAY
+
+
+def obj_to_bin(obj):
+    # Input : a watch part
+    # Output : the binary version of this part
+    code = "2"
+    if obj.__class__.__name__ == "Gear":
+        code += GEAR_CODE
+    elif obj.__class__.__name__ == "EscapeWheel":
+        code += ESCAPE_WHEEL_CODE
+    elif obj.__class__.__name__ == "Barrel":
+        code += BARREL_CODE
+    elif obj.__class__.__name__ == "BalanceWheel":
+        code += BALANCE_WHEEL_CODE
+    elif obj.__class__.__name__ == "Spring":
+        code += SPRING_CODE
+    elif obj.__class__.__name__ == "Hand":
+        code += HAND_CODE
+    elif obj.__class__.__name__ == "Fork": 
+        code += FORK_CODE
+    else:
+        code += BUG_CODE
+
+    code += (11-len(bin(obj.x_position)[2::]))*"0"+bin(obj.x_position)[2::]
+    code += (11-len(bin(obj.y_position)[2::]))*"0"+bin(obj.y_position)[2::]
+    code += (4-len(bin(obj.z_position)[2::]))*"0"+bin(obj.z_position)[2::]
+    if isinstance(obj, Gear):
+        code += (10-len(bin(obj.nb_teeth)[2::]))*"0"+bin(obj.nb_teeth)[2::]
+    return code
+
+
+def bin_to_obj(string):
+    # Input : a binary string
+    # Output : the object version
+    for i in range(4):
+        if string[i] == "2":
+            string = list(string)
+            string[i] = str(rnd.randint(0, 1))
+            string = ''.join(string)
+    if string[0:4] == GEAR_CODE:
+        obj = Gear()
+    elif string[0:4] == ESCAPE_WHEEL_CODE:
+        obj = EscapeWheel()
+    elif string[0:4] == BARREL_CODE:
+        obj = Barrel()
+    elif string[0:4] == BALANCE_WHEEL_CODE:
+        obj = BalanceWheel()
+    elif string[0:4] == SPRING_CODE:
+        obj = Spring()
+    elif string[0:4] == HAND_CODE:
+        obj = Hand()
+    elif string[0:4] == FORK_CODE:
+        obj = Fork()
+    else:
+        obj = Object()
+
+    for i in range(4, 30):
+        if string[i] == "2":
+            string = list(string)
+            string[i] = str(rnd.randint(0, 1))
+            string = ''.join(string)
+    obj.x_position = int(string[4:15], 2)
+    obj.y_position = int(string[15:26], 2)
+    obj.z_position = int(string[26:30], 2)
+
+    if isinstance(obj, Gear):
+        if len(string) < 40:
+            return Object()
+        for i in range(30, 40):
+            if string[i] == "2":
+                string = list(string)
+                string[i] = str(rnd.randint(0, 1))
+                string = ''.join(string)
+        obj.nb_teeth = int(string[30:40], 2)
+    return obj
+
+
+def watch_to_bin(watch: tuple):
+    # INPUT : a watch (list of objects)
+    # OUTPUT : the converted watch in a pseudo-binary format
+    string = ''
+    for i in range(1, len(watch)):
+        string += obj_to_bin(watch[i])
+    return string
+
+
+def bin_to_watch(string, number):
+    # INPUT : a converted watch in a pseudo-binary format
+    # OUTPUT : the watch (list of objects)
+    watch = [number]
+    for i in range(len(string)):
+        if string[i] == "2" and len(string[i+1::]) >= 30:
+            obj = bin_to_obj(string[i+1::])
+            watch += [obj]
+    return watch
+
+
+classes = (Gear, EscapeWheel, Barrel, BalanceWheel, Spring, Hand, Fork)
+
+
+def generate(nb_monsters, min_obj, max_obj):
+    # Input : the number of watches wanted, plus the min and max number of parts in these watches
+    # Output : a list of random watches respecting the inputs
+    generation = [[] for _ in range(nb_monsters)]
+    for i in range(nb_monsters):
+        generation[i] = [i]
+        for j in range(rnd.randint(min_obj, max_obj)):
+            generation[i] += [rnd.choice(classes)()]
+    return generation
+
+
+def mate(watch1, watch2, number):
+    # returns the baby of watch1 and watch2 as another watch. Ask for a number for the first element of the watch
+    component_list_1 = watch_to_bin(watch1).split("2")
+    component_list_2 = watch_to_bin(watch2).split("2")
+    baby_string = ""
+
+    for i in range(rnd.randint(int(len(component_list_1)/4), int(len(component_list_1)*3/4))):
+        baby_string += "2" + component_list_1.pop(rnd.randrange(len(component_list_1)))
+
+    for i in range(rnd.randint(int(len(component_list_2)/4), int(len(component_list_2)*3/4))):
+        baby_string += "2" + component_list_2.pop(rnd.randrange(len(component_list_2)))
+
+    baby_watch = bin_to_watch(baby_string, number)
+
+    return baby_watch
+
+
+def mutate(code):
+    code = list(code)
+    for i in range(len(code)):
+        if rnd.random()+CHANCE_TO_MUTATE >= 1:
+            #print('mutation')
+            code[i] = str((int(code[i]) + rnd.randint(1, 2)) % 3)
+    return ''.join(code)
+
+
+def distance_between(obj1: Object, obj2: Object):
+    """
+    It gives the horizontal distance between 2 objects
+    """
+    return m.sqrt((obj1.x_position - obj2.x_position) ** 2 + (obj1.y_position - obj2.y_position) ** 2)
+
+
+def connect(obj1, obj2):
+    # Return the connexion of obj1 and obj2 [obj1, obj2, type_of_connection]
+    # Types can be : XY_CONNECTION, Z_CONNECTION, or NO_CONNECTION
+    distance = distance_between(obj1, obj2)
+
+    if obj1.z_position == obj2.z_position and distance_between(obj1, obj2) < (obj1.ray + obj2.ray):
+        return [obj1, obj2, XY_CONNECTION]
+
+    elif obj1.z_position != obj2.z_position and distance_between(obj1, obj2) < PIN_DISTANCE:
+        return [obj1, obj2, Z_CONNECTION]
+
+    else:
+        return [obj1, obj2, NO_CONNECTION]
+
+
+def list_connections(watch):
+    """
+    Returns a list of trinome for every connection in the commited watch using this convention : [object_1, object_2, type_of_connection]
+    type of connection can be XY_CONNECTION or Z_CONNECTION
+    """
+    connections = []
+    for i in range(1, len(watch)):
+        for j in range(i+1, len(watch)):
+            connection = connect(watch[i], watch[j])
+            if connection[2] != NO_CONNECTION:
+                connections += [connection]
+    return connections
